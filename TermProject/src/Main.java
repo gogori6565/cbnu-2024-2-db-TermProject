af4111b42bb841a441623bd29fb23b89afabe658
Juhyeon Lee <81895590+gogori6565@users.noreply.github.com>
2024-12-08 22:32:11 +0900

    club_management 데이터베이스 연결

--- TermProject/src/Main.java
+++ TermProject/src/Main.java
@@ -1,6 +1,22 @@
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String args[]) {
+        try {
+            // Mysql JDBC 드라이버 로드
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
+            // Mysql 데이터베이스 연결
+            Connection con = DriverManager.getConnection(
+                    "[url]",
+                    "[username]", "[password]");
+            Statement stmt = con.createStatement();
+            Scanner scanner = new Scanner(System.in);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
